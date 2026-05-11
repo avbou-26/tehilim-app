59c6cf17e827a55c2927f56400d7bb65e69a5b73
avraham bouaziz <avraham.bouaziz@ozar.fr>
2026-05-11 23:54:33 +0200

    update

--- src/Groupe.js
+++ src/Groupe.js
@@ -9,7 +9,7 @@ function getTodayKey() {
 }
 
 const isMobile = window.innerWidth < 600;
-const PRENOMS_MULTIPLES = ['chalva', 'avraham', 'esther j'];
+const PRENOMS_MULTIPLES = ['chalva', 'avraham', 'esther', 'esther j'];
 
 function PopupValidé({ couleur }) {
   const [phase, setPhase] = useState(0);
